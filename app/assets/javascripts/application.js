fa88bf44eac618123256c7e581852943bc7e91e3
Steve Molesworth <>
2024-04-17 12:08:39 +0100

    wayfinding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,17 @@ window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 
 
+  document.getElementById('toggleLink').addEventListener('click', function() {
+    var designNotes = document.querySelectorAll('.designNote'); // Get all elements with class 'designNote'
+    for (var i = 0; i < designNotes.length; i++) {
+        if (designNotes[i].style.display === 'none') {
+            designNotes[i].style.display = 'block'; // Show the designNote element
+        } else {
+            designNotes[i].style.display = 'none'; // Hide the designNote element
+        }
+    }
+  });
+
   
 
 })
